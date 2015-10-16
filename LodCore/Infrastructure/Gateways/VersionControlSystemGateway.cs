d33d3dd04d59883ea65a00f728bdac8e7924334a
Sovent <vmargelov@gmail.com>
2015-10-17 01:52:59 +0300

    Tests are started to get written

--- LodCore/Infrastructure/Gateways/VersionControlSystemGateway.cs
+++ LodCore/Infrastructure/Gateways/VersionControlSystemGateway.cs
@@ -1,7 +1,25 @@
-﻿namespace Gateways
+﻿using System;
+using ProjectManagement.Application;
+using ProjectManagement.Domain;
+using ProjectManagement.Infrastructure;
+
+namespace Gateways
 {
-    public class VersionControlSystemGateway
+    public class VersionControlSystemGateway : IVersionControlSystemGateway
     {
-         
+        public Uri CreateRepositoryForProject(CreateProjectRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUserToProject(Project project, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUserFromProject(Project project, int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ LodCore/ProjectManagementTests/ProjectProviderTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NotificationService;
+using ProjectManagement.Domain;
+using ProjectManagement.Domain.Events;
+using ProjectManagement.Infrastructure;
+
+namespace ProjectManagementTests
+{
+    [TestClass]
+    public class ProjectProviderTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            _pmGateway = new Mock<IProjectManagerGateway>();
+            _repository = new Mock<IProjectRepository>();
+            _vcsGateway = new Mock<IVersionControlSystemGateway>();
+
+            _projectProvider = new ProjectProvider(
+                _pmGateway.Object, 
+                _vcsGateway.Object, 
+                _repository.Object, 
+                new ProjectsEventSink(
+                    Mock.Of<IEventRepository>(), 
+                    Mock.Of<IDistributionPolicyFactory>()));
+
+        }
+
+        [TestMethod]
+        public void ProjectHasToBeAddedSuccessfully()
+        {
+        }
+
+        private Mock<IProjectManagerGateway> _pmGateway;
+        private Mock<IProjectRepository> _repository;
+        private Mock<IVersionControlSystemGateway> _vcsGateway;
+        private ProjectProvider _projectProvider;
+    }
+}
