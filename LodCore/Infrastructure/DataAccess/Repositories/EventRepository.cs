97e5e4a426ed26b9d104cba81d95b2947cd7da61
Sovent <vmargelov@gmail.com>
2015-10-24 11:33:09 +0300

    EventRepository first implementtion

--- LodCore/Infrastructure/DataAccess/Repositories/EventRepository.cs
+++ LodCore/Infrastructure/DataAccess/Repositories/EventRepository.cs
@@ -1,7 +1,22 @@
-﻿namespace DataAccess.Repositories
+﻿using NotificationService;
+
+namespace DataAccess.Repositories
 {
-    public class EventRepository
+    public class EventRepository : IEventRepository
     {
-         
+        public void DistrubuteEvent(Event @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Event[] GetEventsByUser(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MarkEventsAsRead(int[] eventIds)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
