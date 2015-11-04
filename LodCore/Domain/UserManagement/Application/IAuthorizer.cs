b126b6e2d916dea0136ff0c96aad29cae88a26cd
Sovent <vmargelov@gmail.com>
2015-11-05 00:15:16 +0300

    Authorization interface

---
+++ LodCore/Domain/UserManagement/Application/IAuthorizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserManagement.Application
+{
+    public interface IAuthorizer
+    {
+        bool CheckAuthorized(string authorizationToken);
+
+        string Authorize(string email, string password);
+
+        TimeSpan TokenLifeTime { get; }
+    }
+}
