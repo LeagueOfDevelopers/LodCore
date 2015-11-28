c17b931ea929eae1cdd06f0f4701aa4a1e4b6862
valdman <boris.valdman@live.ru>
2015-11-28 17:08:41 +0300

    Убран хардмод из MailerSettings, удалены ненужные нереализованные тесты

--- LodCore/Mailing/MailerSettings.cs
+++ LodCore/Mailing/MailerSettings.cs
@@ -2,12 +2,22 @@
 {
     public class MailerSettings
     {
-        public string SmtpServer { get; } = "217.69.139.160";
-        public int Port { get; } = 465;
-        public string Password { get; } = "F0rjustice";
+        public MailerSettings(string smtpServer, int port, string password, string @from, string caption, string messageTemplate)
+        {
+            SmtpServer = smtpServer;
+            Port = port;
+            Password = password;
+            From = @from;
+            Caption = caption;
+            MessageTemplate = messageTemplate;
+        }
 
-        public string From { get; } = @"leagueofdevelopers@mail.ru";
-        public string MessageTemplate { get; } = ValidationMessageResources.messageTemplate;
-        public string Caption { get; } = "Подтверждение аккаунта на сайте Лиги Разработчиков";
+        public string SmtpServer { get; }// = "217.69.139.160";
+        public int Port { get; }// = 465;
+        public string Password { get; }// = "F0rjustice";
+
+        public string From { get; }// = @"leagueofdevelopers@mail.ru";
+        public string MessageTemplate { get; }// = ValidationMessageResources.messageTemplate;
+        public string Caption { get; }// = "Подтверждение аккаунта на сайте Лиги Разработчиков";
     }
 }
