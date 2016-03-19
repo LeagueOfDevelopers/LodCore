2bd93201d206dac1443ffa70a21e6b828ef8f456
valdman <boris.valdman@live.ru>
2016-03-19 19:51:30 +0300

    Work on task begin

---
+++ LodCore/Domain/NotificationService/NotificationEventSink.cs
@@ -0,0 +1,23 @@
+﻿using Journalist;
+using UserPresentaton;
+
+namespace NotificationService
+{
+    public class NotificationEventSink : EventSinkBase
+    {
+        public NotificationEventSink(IDistributionPolicyFactory distributionPolicyFactory, IEventRepository eventRepository, IMailer mailer, IUserPresentationProvider userPresentationProvider) : base(distributionPolicyFactory, eventRepository, mailer, userPresentationProvider)
+        {
+        }
+
+        public override void ConsumeEvent(IEventInfo eventInfo)
+        {
+            Require.NotNull(eventInfo, nameof(eventInfo));
+
+            var @event = new Event(eventInfo);
+
+            var distributionPolicy = DistributionPolicyFactory.GetAllPolicy();
+
+            EventRepository.DistrubuteEvent(@event, distributionPolicy);
+        }
+    }
+}
