aa93e5df9f20856ab769b54b5c270d7af3750cc9
Helenga <burlena23@gmail.com>
2017-12-09 01:26:24 +0300

    fixed

---
+++ LodCore/Application/FrontendServices/Migrations/20171208011351_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FrontendServices.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    EventInfo = table.Column<string>(nullable: true),
+                    EventType = table.Column<string>(nullable: true),
+                    OccuredOn = table.Column<DateTime>(nullable: false),
+                    WasRead = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
